94dbc5a9d05b818a1e02a0e5d56f180f79ad6a72
gtt116 <gtt116@gmail.com>
2026-05-28 00:29:24 +0800

    feat: enable skills toolset for AIAgent sessions

--- enikk/eternity.py
+++ enikk/eternity.py
@@ -149,7 +149,7 @@ class Eternity:
             api_key=mc.api_key or None,
             model=model or mc.default,
             max_tokens=mc.max_tokens,
-            enabled_toolsets=[GameController.TOOLSET],
+            enabled_toolsets=[GameController.TOOLSET, "skills"],
             quiet_mode=False,
             save_trajectories=False,
             max_iterations=max_iterations,
